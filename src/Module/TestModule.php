85884de0329930f293f8d6d49baeab381666c758
Sven-Uwe Bruch <subruch@ansys.de>
2021-08-03 13:49:56 +0200

    update

---
+++ src/Module/TestModule.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace AnSyS\TestBundle\Module;
+
+class TestModule extends \Module
+{
+    /**
+     * @var string
+     */
+    protected $strTemplate = 'mod_test';
+
+    /**
+     * Displays a wildcard in the back end.
+     *
+     * @return string
+     */
+    public function generate()
+    {
+        if (TL_MODE == 'BE') {
+            $template = new \BackendTemplate('be_wildcard');
+
+            $template->wildcard = '### '.utf8_strtoupper($GLOBALS['TL_LANG']['FMD']['test'][0]).' ###';
+            $template->title = $this->headline;
+            $template->id = $this->id;
+            $template->link = $this->name;
+            $template->href = 'contao/main.php?do=themes&amp;table=tl_module&amp;act=edit&amp;id='.$this->id;
+
+            return $template->parse();
+        }
+
+        return parent::generate();
+    }
+
+    /**
+     * Generates the module.
+     */
+    protected function compile()
+    {
+        $this->Template->message = 'Hello World';
+    }
+}
---
+++ src/Resources/contao/languages/de/modules.php
@@ -0,0 +1,4 @@
+<?php
+
+// Frontend modules
+$GLOBALS['TL_LANG']['FMD']['test'] = ['Testmodul', 'Gibt den Text "Hello World" aus'];
