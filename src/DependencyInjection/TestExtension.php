de3acb86548d2f289d494e4350491c15efb234ce
Sven-Uwe Bruch <subruch@ansys.de>
2021-08-03 13:58:04 +0200

    Bugfix

--- src/DependencyInjection/TestExtension.php
+++ src/DependencyInjection/TestExtension.php
@@ -1,14 +1,5 @@
 <?php
-
-/*
- * This file is part of [package name].
- *
- * (c) John Doe
- *
- * @license LGPL-3.0-or-later
- */
-
-namespace Contao\SkeletonBundle\DependencyInjection;
+namespace AnSyS\TestBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
